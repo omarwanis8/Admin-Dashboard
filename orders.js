040bd279ad57847d81e9ea39904603ff4b51628b
omarwanis8 <149831398+omarwanis8@users.noreply.github.com>
2024-04-13 18:11:55 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,51 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drome',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+   {
+       productName: 'LARVENDER KF102 DRONE',
+       productNumber: '36378',
+       paymentStatus: 'Refunde',
+       shipping: 'Declined'
+   },
+
+   {
+    productName: 'Ruko Fu Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+   },
+
+   {
+    productName: 'Drone with Camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+   },
+
+   {
+    productName: 'GPS 4k Drone',
+     productNumber: '2282f',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+   },
+
+   {
+    productName: 'DJI Air zS',
+    productNumber: '81475',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+   },
+
+   {
+   productName: 'Lozenge Drone',
+   productNumber: '00482',
+   paymentStatus: 'Paid',
+   shipping: 'Delivered'
+   }
+
+]
